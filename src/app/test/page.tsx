34d18242e5844fe1ec1e5dfdb03b309807d016a0
HangA-Kim <foxbaboo@google.com>
2024-11-08 12:36:45 +0900

    warning del

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import { LineChartAnalysis } from "~/app/_components/chart/LineChartAnalysis";
 const test = async () => {
   return (
     <Box sx={{ alignContent: "center", width: "100%" }}>
-      <LineChartAnalysis/>
     </Box>
   );
 };
